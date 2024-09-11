d66e84c590688059a8fde058d6e48b68050aacc1
Ytzhak <ytzhakcc@gmail.com>
2024-09-11 14:28:39 -0500

    Cambio / Monte Carlo

--- Ciclo 2 Python/.py/SRV/09-09-2024 Monte Carlo Simulation.py
+++ Ciclo 2 Python/.py/SRV/09-09-2024 Monte Carlo Simulation.py
@@ -9,6 +9,7 @@ for i in range(totalPoints):
     y = random.uniform(-1, 1)
     if x**2 + y**2 <= 1:
         pointsInCircle += 1
+        
 pi_estimate = 4 * pointsInCircle / totalPoints
 print(f"Estimado de pi: {pi_estimate}")
 print(f"Puntos que cayeron en el círculo: {pointsInCircle:,}")
