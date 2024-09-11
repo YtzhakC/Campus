fc8f46f9559e421b47cc14b5451e155c27c270fa
Ytzhak <ytzhakcc@gmail.com>
2024-09-11 18:51:00 -0500

    Si

---
+++ Ciclo 2 Python/.py/EXTRAS/comandos cadenas.py
@@ -0,0 +1,6 @@
+c1 = "Intento de cadena"
+c2 = "Cadena más grande"
+
+print(c1.capitalize()) # Interpreta la cadena y agrega mayuscula en  la primera letra de la sentencia
+print(c1.lower()) # Pasa todo a minúsculas
+print(c1.upper()) # Pasa todo a mayúsculas3
